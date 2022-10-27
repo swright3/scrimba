292cd3665d91a9e7f567c70a07c0f25b4a95dce6
sam <samwrighthwk@gmail.com>
2022-10-27 21:20:02 +0100

    Finished info part of card

--- projects/business_card/src/components/About.js
+++ projects/business_card/src/components/About.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function About() {
     return (
         <div className='About'>
-            
+            <h2>About</h2>
+            <p>It's a me, Mariommmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm</p>
         </div>
     )
 }
--- projects/business_card/src/components/Info.js
+++ projects/business_card/src/components/Info.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import me from '../images/me.jpg'
+import email from '../images/email.png'
+import linkedin from '../images/linkedin.png'
 
 export default function Info() {
     return (
         <div className='Info'>
-
+            <img src={me} width="300px" height="auto" className='profilepic'/>
+            <h1>Sam Wright</h1>
+            <p className='title'>Full Stack Developer</p>
+            <div className='buttons'>
+                <a href='mailto:samwrighthwk@gmail.com' className='email'>
+                    <img src={email} width="10px" height="auto" className='emailpic'/>
+                    <p className='emailtext'>Email</p>
+                </a>
+                <a href='https://www.linkedin.com/in/swright31/' className='linkedin'>
+                    <img src={linkedin} width="10px" height="auto" className='linkedinpic' />
+                </a>
+            </div>
         </div>
     )
 }
