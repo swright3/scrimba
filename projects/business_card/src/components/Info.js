1c3e54702499d4906f75d239d07d7c9fc6ffc8af
Sam <samwrighthwk@gmail.com>
2022-11-03 13:08:56 +0000

    Detailing contact buttons

--- projects/business_card/src/components/Info.js
+++ projects/business_card/src/components/Info.js
@@ -3,6 +3,7 @@ import InfoCSS from './Info.module.css'
 import me from '../images/me.jpg'
 import email from '../images/email.png'
 import linkedin from '../images/linkedin.png'
+import linkedinButton from '../images/linkedin.png'
 
 export default function Info() {
     return (
@@ -11,12 +12,8 @@ export default function Info() {
             <h1 className={InfoCSS.name}>Sam Wright</h1>
             <p className={InfoCSS.title}>Full Stack Developer</p>
             <div className={InfoCSS.buttons}>
-                <a href='mailto:samwrighthwk@gmail.com' className={InfoCSS.email}>
-                    email
-                </a>
-                <a href='https://www.linkedin.com/in/swright31/' className={InfoCSS.linkedin}>
-                    linkedIn
-                </a>
+                <a href='mailto:samwrighthwk@gmail.com' className={InfoCSS.email}></a>
+                <a href='https://www.linkedin.com/in/swright31/' className={InfoCSS.linkedin}></a>
             </div>
         </div>
     )
