233ac9902243869626a20a04d1549e72d672953c
Sam <samwrighthwk@gmail.com>
2022-11-03 13:21:39 +0000

    Finished detailing buttons

--- projects/business_card/src/components/Info.js
+++ projects/business_card/src/components/Info.js
@@ -2,8 +2,6 @@ import React from 'react'
 import InfoCSS from './Info.module.css'
 import me from '../images/me.jpg'
 import email from '../images/email.png'
-import linkedin from '../images/linkedin.png'
-import linkedinButton from '../images/linkedin.png'
 
 export default function Info() {
     return (
@@ -12,7 +10,12 @@ export default function Info() {
             <h1 className={InfoCSS.name}>Sam Wright</h1>
             <p className={InfoCSS.title}>Full Stack Developer</p>
             <div className={InfoCSS.buttons}>
-                <a href='mailto:samwrighthwk@gmail.com' className={InfoCSS.email}></a>
+                <a href='mailto:samwrighthwk@gmail.com' className={InfoCSS.email}>
+                    <div className={InfoCSS.emailContent}>
+                        <img src={email} width='50px'/>
+                        <p>Email</p>
+                    </div>
+                </a>
                 <a href='https://www.linkedin.com/in/swright31/' className={InfoCSS.linkedin}></a>
             </div>
         </div>
