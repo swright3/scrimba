5bb8448c6afa6f54bcf57a32bd5e8119b578dfeb
Sam <samwrighthwk@gmail.com>
2022-11-08 14:30:04 +0000

    Fixed vertical growth of descriptions and updated styles and data

--- projects/travel_journal/src/data.js
+++ projects/travel_journal/src/data.js
@@ -5,7 +5,7 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/wjQmA9YAQcBjJiuz8",
         startDate: "30 Mar, 2022",
         endDate: "2 Apr, 2022",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        description: "My first ever solo traveling experience. My flight back from the climbing tour in Sicily was cancelled so I decided to make the most of it and fly to Northern Italy. I had a lot of fun getting lost around the canals, and I had a good taste of culture when I saw a performance of Vivaldi's Four Seasons.",
         imageUrl: "../images/venice.jpg"
     },
     {
@@ -14,7 +14,7 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/7ib7ZPU8rbuEFBru5",
         startDate: "7 Apr, 2022",
         endDate: "11 Apr, 2022",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        description: "Athens was my next stop after Italy. Here I met my old housemate Alexi who really took the role of tour guide seriously. I must've seen the city from nearly every single vantage point possible, and yes Alexi, I can smell the orange blossom!",
         imageUrl: "../images/athens.jpg"
     },
     {
@@ -23,7 +23,7 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/hT8oGvcLyfH8qi9s8",
         startDate: "11 Apr, 2022",
         endDate: "20 Apr, 2022",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        description: "Here I met a lot of new people thanks to my Roman and Joe. It didn't take long for them to convince me to move out there and look for a job. Berlin has an amazing club/rave scene and so many interesting people to talk to.",
         imageUrl: "../images/berlin.jpg"
     },
     {
@@ -32,7 +32,7 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/qLTEFfTCqJdQ7drN9",
         startDate: "20 Apr, 2022",
         endDate: "24 Apr, 2022",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        description: "Only after booking the tickets did I realise I was arriving on 4/20 ;). Here I met my friends Loki and Charlie and we had a great time, especially on the boat tour and when we rented bikes.",
         imageUrl: "../images/amsterdam.jpg"
     },
     {
@@ -41,7 +41,7 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/F4kbDRn8FgKrETCQ6",
         startDate: "03 Sep, 2022",
         endDate: "12 Sep, 2022",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        description: "I booked this holiday when I was visiting Berlin and I'm super glad I did. A big group of us stayed in an Airbnb and had a chilled out, beach filled getaway. I really enjoyed gathering oysters and shucking them on the beach.",
         imageUrl: "../images/krka.jpg"
     }
 ]
