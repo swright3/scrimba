0a248d3cc858bac3ebed6aeb9cb2ab42c2d69bb6
Sam <samwrighthwk@gmail.com>
2022-11-12 12:11:40 +0000

    Changed numbers to dots

--- projects/tenzies/src/Die.js
+++ projects/tenzies/src/Die.js
@@ -2,16 +2,70 @@ import React from "react"
 
 export default function Die(props) {
     const styles = {
-        backgroundColor: props.isHeld ? "white" : "#c20606",
-        color: props.isHeld ? "#c20606" : "white"
+        color: props.isHeld ? "white" : "#c20606",
+        backgroundColor: props.isHeld ? "#c20606" : "white"
     }
+
+    function getDots() {
+        switch (props.value) {
+            case 1:
+                return (
+                    <div className="dots">
+                    <div></div><div></div><div></div>
+                    <div></div><div>&#8226;</div><div></div>
+                    <div></div><div></div><div></div>
+                    </div>
+                )
+            case 2:
+                return (
+                    <div className="dots">
+                    <div>&#8226;</div><div></div><div></div>
+                    <div></div><div></div><div></div>
+                    <div></div><div></div><div>&#8226;</div>
+                    </div>
+                )
+            case 3:
+                return (
+                    <div className="dots">
+                    <div>&#8226;</div><div></div><div></div>
+                    <div></div><div>&#8226;</div><div></div>
+                    <div></div><div></div><div>&#8226;</div>
+                    </div>
+                )
+            case 4:
+                return (
+                    <div className="dots">
+                    <div>&#8226;</div><div></div><div>&#8226;</div>
+                    <div></div><div></div><div></div>
+                    <div>&#8226;</div><div></div><div>&#8226;</div>
+                    </div>
+                )
+            case 5:
+                return (
+                    <div className="dots">
+                    <div>&#8226;</div><div></div><div>&#8226;</div>
+                    <div></div><div>&#8226;</div><div></div>
+                    <div>&#8226;</div><div></div><div>&#8226;</div>
+                    </div>
+                )
+            case 6:
+                return (
+                    <div className="dots">
+                    <div>&#8226;</div><div></div><div>&#8226;</div>
+                    <div>&#8226;</div><div></div><div>&#8226;</div>
+                    <div>&#8226;</div><div></div><div>&#8226;</div>
+                    </div>
+                )
+        }
+    }
+
     return (
         <div 
             className="die-face" 
             style={styles}
             onClick={props.holdDice}
         >
-            <h2 className="die-num">{props.value}</h2>
+            {getDots()}
         </div>
     )
 }
