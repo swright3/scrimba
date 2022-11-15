d5f5165d08fd5b1744ad60a9f33ce8259ae2e348
Sam <samwrighthwk@gmail.com>
2022-11-15 00:43:02 +0000

    Mocked up design and started intro page

--- projects/trivia/src/App.js
+++ projects/trivia/src/App.js
@@ -1,8 +1,17 @@
-import './App.css';
+import AppCSS from './App.module.css';
+import questionMarkBg from './images/question-mark-bg.png'
+import exclamationMarkBg from './images/exclamation-mark-bg.png'
 
 function App() {
   return (
-    <div>ji</div>
+    <div className={AppCSS.App}>
+      <img src={questionMarkBg} alt='question mark' width='200px' height='200px' id={AppCSS.questionMark}/>
+      <img src={exclamationMarkBg} alt='exclamation mark' width='200px' height='200px' id={AppCSS.exclamationMark}/>
+      <div className={AppCSS.intro}>
+        <h1>Quizzical</h1>
+        <h2>The randomized trivia app</h2>
+      </div>
+    </div>
   );
 }
 
