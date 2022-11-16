a5cfc79e94c4e976389b856e1ca84ea60da8241d
Sam <samwrighthwk@gmail.com>
2022-11-16 18:52:21 +0000

    Added a form and animation to the title screen, form not fully functioning

--- projects/trivia/src/Intro.js
+++ projects/trivia/src/Intro.js
@@ -1,14 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 import IntroCSS from './Intro.module.css'
 
 export default function Intro(props) {
+    const [formData, setFormData] = useState({
+        name: '',
+        difficulty: 'mixed',
+        category: 'any'
+    })
     const {startQuiz} = props
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setFormData(prevData => ({
+            ...prevData,
+            [name]: value
+        }))
+    }
 
     return (
         <div className={IntroCSS.Intro}>
-            <h1>Quizzical</h1>
-            <h2>The randomized trivia app</h2>
-            <button onClick={startQuiz}>Start Quiz!</button>
+            <div className={`${IntroCSS.title} ${IntroCSS.titleAnimation}`}>
+                <h1>Quizzical</h1>
+                <h2>The randomized trivia app</h2>
+            </div>
+            <form>
+                <input type='text' placeholder='Name' onChange={handleChange} name='name' value={formData.name}/>
+                <select id='difficulty' value={formData.difficulty} onChange={handleChange} name='difficulty'>
+                    <option value='mixed'>Mixed</option>
+                    <option value='easy'>Easy</option>
+                    <option value='medium'>Medium</option>
+                    <option value='hard'>Hard</option>
+                </select>
+                <select id='category' value={formData.category} onChange={handleChange} name='category'>
+                    <option value='any'>All Categories</option>
+                    <option value='generalKnowledge'>General Knowledge</option>
+                    <option value='scienceAndNature'>Science & Nature</option>
+                    <option value='sports'>Sports</option>
+                </select>
+                <button onClick={startQuiz}>Start Quiz!</button>
+            </form>
         </div>
     )
 }
